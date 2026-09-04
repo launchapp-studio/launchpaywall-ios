33a5b11c0245f334bb02b2c830d5c11e8454a66e
LaunchApp Studio <hello.launchapp.studio@gmail.com>
2026-09-04 02:23:02 +0200

    Initial Commit

---
+++ LaunchPaywall/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LaunchPaywall
+//
+//  Created by Javier Mazorra Rodríguez on 01/09/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LaunchPaywall/LaunchPaywallApp.swift
@@ -0,0 +1,17 @@
+//
+//  LaunchPaywallApp.swift
+//  LaunchPaywall
+//
+//  Created by Javier Mazorra Rodríguez on 01/09/2026.
+//
+
+import SwiftUI
+
+@main
+struct LaunchPaywallApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
